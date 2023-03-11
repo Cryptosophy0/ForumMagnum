be361bb16e6343e3cdf93fd296883b24ed941b47
astrum5000 <astrum5000@gmail.com>
2023-03-10 21:41:01 -0800

    update run production to use $SETTINGS_FILE_NAME

--- scripts/runProduction.sh
+++ scripts/runProduction.sh
@@ -21,4 +21,4 @@ echo "Running Production Site"
 # fi
 
 export NODE_OPTIONS="--max_old_space_size=2560 --heapsnapshot-signal=SIGUSR2"
-./build.js -run --settings ./settings-prod.json --production
+./build.js -run --settings $SETTINGS_FILE_NAME --production
