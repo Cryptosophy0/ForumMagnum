12640d2b4033373a7839ffeee429d1ab8ee7fbd1
astrum5000 <astrum5000@gmail.com>
2023-03-11 12:31:12 -0800

    add icons

---
+++ packages/lesswrong/components/icons/heliosIcon.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export const heliosIcon = <svg viewBox="0 0 420 420" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="210" cy="10" r="10" fill="#696969"/>
+<circle cx="249.018" cy="13.843" r="10" transform="rotate(11.25 249.018 13.843)" fill="#696969"/>
+<circle cx="286.537" cy="25.2241" r="10" transform="rotate(22.5 286.537 25.2241)" fill="#696969"/>
+<circle cx="321.114" cy="43.7061" r="10" transform="rotate(33.75 321.114 43.7061)" fill="#696969"/>
+<circle cx="351.421" cy="68.5787" r="10" transform="rotate(45 351.421 68.5787)" fill="#696969"/>
+<circle cx="376.294" cy="98.8859" r="10" transform="rotate(56.25 376.294 98.8859)" fill="#696969"/>
+<circle cx="394.776" cy="133.463" r="10" transform="rotate(67.5 394.776 133.463)" fill="#696969"/>
+<circle cx="406.157" cy="170.982" r="10" transform="rotate(78.75 406.157 170.982)" fill="#696969"/>
+<circle cx="410" cy="210" r="10" transform="rotate(90 410 210)" fill="#696969"/>
+<circle cx="406.157" cy="249.018" r="10" transform="rotate(101.25 406.157 249.018)" fill="#696969"/>
+<circle cx="394.776" cy="286.537" r="10" transform="rotate(112.5 394.776 286.537)" fill="#696969"/>
+<circle cx="376.294" cy="321.114" r="10" transform="rotate(123.75 376.294 321.114)" fill="#696969"/>
+<circle cx="351.421" cy="351.421" r="10" transform="rotate(135 351.421 351.421)" fill="#696969"/>
+<circle cx="321.114" cy="376.294" r="10" transform="rotate(146.25 321.114 376.294)" fill="#696969"/>
+<circle cx="286.537" cy="394.776" r="10" transform="rotate(157.5 286.537 394.776)" fill="#696969"/>
+<circle cx="249.018" cy="406.157" r="10" transform="rotate(168.75 249.018 406.157)" fill="#696969"/>
+<circle cx="210" cy="410" r="10" transform="rotate(180 210 410)" fill="#696969"/>
+<circle cx="170.982" cy="406.157" r="10" transform="rotate(-168.75 170.982 406.157)" fill="#696969"/>
+<circle cx="133.463" cy="394.776" r="10" transform="rotate(-157.5 133.463 394.776)" fill="#696969"/>
+<circle cx="98.886" cy="376.294" r="10" transform="rotate(-146.25 98.886 376.294)" fill="#696969"/>
+<circle cx="68.5786" cy="351.421" r="10" transform="rotate(-135 68.5786 351.421)" fill="#696969"/>
+<circle cx="43.706" cy="321.114" r="10" transform="rotate(-123.75 43.706 321.114)" fill="#696969"/>
+<circle cx="25.224" cy="286.537" r="10" transform="rotate(-112.5 25.224 286.537)" fill="#696969"/>
+<circle cx="13.843" cy="249.018" r="10" transform="rotate(-101.25 13.843 249.018)" fill="#696969"/>
+<circle cx="10" cy="210" r="10" transform="rotate(-90 10 210)" fill="#696969"/>
+<circle cx="13.843" cy="170.982" r="10" transform="rotate(-78.75 13.843 170.982)" fill="#696969"/>
+<circle cx="25.2241" cy="133.463" r="10" transform="rotate(-67.5 25.2241 133.463)" fill="#696969"/>
+<circle cx="43.7061" cy="98.8859" r="10" transform="rotate(-56.25 43.7061 98.8859)" fill="#696969"/>
+<circle cx="68.5787" cy="68.5786" r="10" transform="rotate(-45 68.5787 68.5786)" fill="#696969"/>
+<circle cx="98.886" cy="43.706" r="10" transform="rotate(-33.75 98.886 43.706)" fill="#696969"/>
+<circle cx="133.463" cy="25.2241" r="10" transform="rotate(-22.5 133.463 25.2241)" fill="#696969"/>
+<circle cx="170.982" cy="13.8429" r="10" transform="rotate(-11.25 170.982 13.8429)" fill="#696969"/>
+<path d="M210.5 98C195.726 98 181.097 100.91 167.448 106.564C153.799 112.217 141.397 120.504 130.95 130.95C120.504 141.397 112.217 153.799 106.564 167.448C100.91 181.097 98 195.726 98 210.5C98 225.274 100.91 239.903 106.564 253.552C112.217 267.201 120.504 279.603 130.95 290.05C141.397 300.496 153.799 308.783 167.448 314.436C181.097 320.09 195.726 323 210.5 323L210.5 210.5L210.5 98Z" fill="#D9D9D9"/>
+</svg>
+
