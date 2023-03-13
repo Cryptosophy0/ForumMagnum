47958d996f4044e2db57a1ab48ceabe29021279c
astrum5000 <astrum5000@gmail.com>
2023-03-12 21:51:54 -0700

    update sendEmail.ts

--- packages/lesswrong/server/emails/sendEmail.ts
+++ packages/lesswrong/server/emails/sendEmail.ts
@@ -2,16 +2,19 @@ import { DatabaseServerSetting } from '../databaseSettings';
 import type { RenderedEmail } from './renderEmail';
 import nodemailer from 'nodemailer';
 
-export const mailUrlSetting = new DatabaseServerSetting<string | null>('mailUrl', null) // The SMTP URL used to send out email
+// export const mailUrlSetting = new DatabaseServerSetting<string | null>('mailUrl', null) // The SMTP URL used to send out email
 
-const getMailUrl = () => {
-  if (mailUrlSetting.get())
-    return mailUrlSetting.get();
-  else if (process.env.MAIL_URL)
-    return process.env.MAIL_URL;
-  else
-    return null;
-};
+// const getMailUrl = () => {
+//   if (mailUrlSetting.get())
+//     return mailUrlSetting.get();
+//   else if (process.env.MAIL_URL)
+//     return process.env.MAIL_URL;
+//   else
+//     return null;
+// };
+
+export const smtpUserSetting = new DatabaseServerSetting<string | null>('mail.smtpUser', null) 
+export const smtpPassSetting = new DatabaseServerSetting<string | null>('mail.smtpPass', null) 
 
 /**
  * Send an email. Returns true for success or false for failure.
@@ -19,15 +22,26 @@ const getMailUrl = () => {
  * API descended from meteor
  */
 export const sendEmailSmtp = async (email: RenderedEmail): Promise<boolean> => {
-  const mailUrl = getMailUrl();
+  const smtpUser = smtpUserSetting.get();
+  const smtpPass = smtpPassSetting.get();
   
-  if (!mailUrl) {
+  if (!smtpUser || !smtpPass) {
     // eslint-disable-next-line no-console
-    console.log("Unable to send email because no mailserver is configured");
+    console.log("Unable to send email because smtp user or smtp pass are not configured");
     return false;
   }
   
-  const transport = nodemailer.createTransport(mailUrl);
+  // const transport = nodemailer.createTransport(mailUrl);
+
+  const transport = nodemailer.createTransport({
+    host: "smtp.mailgun.org",
+    port: 587,
+    secure: false,
+    auth: {
+      user: smtpUser,
+      pass: smtpPass,
+    }
+  });
   
   const result = await transport.sendMail({
     from: email.from,
